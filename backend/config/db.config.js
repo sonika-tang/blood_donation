8977d8c01bcfe46165526a26c6c810b5cc490522
sonika-tang <sonikatang630@gmail.com>
2025-07-28 21:29:56 +0700

    change config file

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  HOST: process.env.PGHOST || 'localhost',
-  USER: process.env.PGUSER || 'postgres',
-  PASSWORD: process.env.PGPASSWORD || 'postgres',
-  DB: process.env.PGDATABASE || 'blood_donation',
-  PORT: process.env.PGPORT || 5432,
+  HOST: process.env.PGHOST,          
+  USER: process.env.PGUSER,          
+  PASSWORD: process.env.PGPASSWORD,  
+  DB: process.env.PGDATABASE,        
+  PORT: process.env.PGPORT,          
   dialect: 'postgres',
   dialectOptions: {
     ssl: process.env.NODE_ENV === 'production' ? {
